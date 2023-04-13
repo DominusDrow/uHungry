1627d5ec2e435c59479c43d68dbe820b44ff73a4
Angel Avendano <avalos_luisangel@hotmail.com>
2023-04-13 12:02:20 -0600

    Poniendo chulo el login

--- screens/ScreenLogin.js
+++ screens/ScreenLogin.js
@@ -1,6 +1,8 @@
 //SCREEN 1
 import { View, StyleSheet, Text, Image, Dimensions} from "react-native";
 import { Button } from '@rneui/themed';
+import { Input } from '@rneui/themed';
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 
 import { useDispatch } from "react-redux";
@@ -29,26 +31,36 @@ const ScreenLogin = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Image style={styles.img} source={require("../assets/img/hungry.jpg")} />
-
-      <Button
-        title="Log in"
-        loading={false}
-        loadingProps={{ size: 'small', color: 'white' }}
-        buttonStyle={{
-          backgroundColor: 'rgba(111, 202, 186, 1)',
-          borderRadius: 5,
-        }}
-        titleStyle={{ fontWeight: 'bold', fontSize: 23 }}
-        containerStyle={{
-          marginHorizontal: 100,
-          height: 50,
-          width: 200,
-          marginVertical: 50,
-        }}
-        onPress={() => onClick()}
-      />
-
+      <View style={styles.cuadrado}>
+        
+      </View>
+      <View style={styles.QLogin}>
+        <Input
+        placeholder='Usuario'
+        leftIcon={<Icon name="account-outline" size={20} />}
+        />
+        <Input
+          placeholder='Contraseña'
+          leftIcon={{ type: 'font-awesome', name: 'chevron-left' }}
+        />
+        <Button
+          title="Log in"
+          loading={false}
+          loadingProps={{ size: 'small', color: 'white' }}
+          buttonStyle={{
+            backgroundColor: '#057A8A',
+            borderRadius: 5,
+          }}
+          titleStyle={{ fontWeight: 'bold', fontSize: 23 }}
+          containerStyle={{
+            position: "absolute",
+            height: 50,
+            width: 200,
+            marginTop: "100%",
+          }}
+          onPress={() => onClick()}
+        />
+      </View>
     </View>
   );
 };
@@ -59,6 +71,26 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
   },
+
+  cuadrado: {
+    height: "100%",
+    marginTop: "80%",
+    backgroundColor: "#057A8A",
+  },
+
+  QLogin: {
+    position:"absolute",
+    borderRadius: 1,
+    shadowColor: '#52006A',  
+    elevation: 20, 
+    width: "80%",
+    height: 400,
+    marginTop: "30%",
+    marginLeft: "10%",
+    backgroundColor: "#FFFFFF",
+    alignItems: 'center',
+  },
+  
   img: {
     width: "80%",
     height: 300,
