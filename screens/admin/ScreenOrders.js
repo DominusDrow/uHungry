d1e1f5eb6f56757265a776ee21d286c07a165cb8
Angel Avendano <avalos_luisangel@hotmail.com>
2023-05-28 13:20:24 -0600

    FlatList no funciona, el item para cada Orden sí(Hay que revisar)

--- screens/admin/ScreenOrders.js
+++ screens/admin/ScreenOrders.js
@@ -5,7 +5,7 @@ import { PEDIDOS} from "../../dummy-data/pedidos";
 
 
 export default function ScreenOrders({ navigation }) {
-  console.log(PEDIDOS.length)
+  console.log(PEDIDOS.length)//OrderItem es el item para mostrar un pedido de la lista PEDIDOS
   return (
     <View style={{ flex: 1, alignItems: 'center',width: Dimensions.get("window").width}}>
       <OrderItem productInfo={PEDIDOS[1]}/>
